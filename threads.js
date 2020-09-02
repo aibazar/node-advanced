c57cd1931b5ce1e9cbeb085ff70416ba31a7a912
stefian <stefan.ianta@w3ai.net>
2020-09-02 15:55:02 -0300

    const crypto = require(crypto);

--- threads.js
+++ threads.js
@@ -1,1 +1,7 @@
-const crypto = require(cripto);
+const crypto = require('crypto');
+
+const start = Date.now();
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('1:', Date.now() - start);
+});
