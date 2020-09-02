ab1ce2dbcb72141f103f9a158a03ed50c73dc35b
stefian <stefan.ianta@w3ai.net>
2020-09-02 15:57:41 -0300

    + crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {   console.log('3:', Date.now() - start); });
    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
      console.log('4:', Date.now() - start);
    });

--- threads.js
+++ threads.js
@@ -9,3 +9,11 @@ crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
   console.log('2:', Date.now() - start);
 });
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('3:', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('4:', Date.now() - start);
+});
