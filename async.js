f63005f77d0b7a3b119009969a62dae9d247ad5a
stefian <stefan.ianta@w3ai.net>
2020-09-02 20:22:32 -0300

    + https.request('https://www.google.com', res => {   res.on('data', () => { });   res.on('end', () => {     console.log(Data.now() - start);   }); }).end();

--- async.js
+++ async.js
@@ -2,3 +2,9 @@ const https = require('https');
 
 const start = Date.now();
 
+https.request('https://www.google.com', res => {
+  res.on('data', () => { });
+  res.on('end', () => {
+    console.log(Data.now() - start);
+  });
+}).end();
