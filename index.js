6b857c02a3214e9a5b5fefc4fbaa2a1d378974d8
stefian <stefan.ianta@w3ai.net>
2020-09-04 10:48:54 -0300

    + // Im a child, im going to act like a server and do nothing else // const express = require('express'); const crypto = require('crypto'); const app = express();
    app.get('/', (req, res) => {
      crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
        res.send('Hi there');
      });
    });
    
    app.get('/fast', (req, res) => {
      res.send('This was fast!')
    });
    
    app.listen(3000);

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-// Change Node Threadpool size from default of 4 //
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require('cluster');
 
-// Is the file being executed in master mode? //
-if (cluster.isMaster) {
-  // Cause index.js to be executed *again* but in child mode // 
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
-} else {
-  // Im a child, im going to act like a server and do nothing else //
-  const express = require('express');
-  const crypto = require('crypto');
-  const app = express();
+// Im a child, im going to act like a server and do nothing else //
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
 
-  app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-      res.send('Hi there');
-    });
+app.get('/', (req, res) => {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    res.send('Hi there');
   });
+});
 
-  app.get('/fast', (req, res) => {
-    res.send('This was fast!')
-  });
+app.get('/fast', (req, res) => {
+  res.send('This was fast!')
+});
 
-  app.listen(3000);
-}
+app.listen(3000);
