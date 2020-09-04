b97180bb7c16b058a7aa2c3e7defebd3ef987e19
stefian <stefan.ianta@w3ai.net>
2020-09-03 23:18:35 -0300

    + // Is the file being executed in master mode? // if (cluster.isMaster) {   // Cause index.js to be executed *again* but in child mode //   cluster.fork(); } else {   // Im a child, im going to act like a server and do nothing else //   const express = require('express');   const app = express();
      function doWork(duration) {
        const start = Date.now();
        while (Date.now() - start < duration) { }
      }
    
      app.get('/', (req, res) => {
        doWork(5000);   // Code executed inside Node event loop //
        res.send('Hi there!');
      });
    
      app.listen(3000);
    }

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const cluster = require('cluster');
 
-const express = require('express');
-const app = express();
+// Is the file being executed in master mode? //
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but in child mode // 
+  cluster.fork();
+} else {
+  // Im a child, im going to act like a server and do nothing else //
+  const express = require('express');
+  const app = express();
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) { }
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) { }
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);   // Code executed inside Node event loop //
+    res.send('Hi there!');
+  });
+
+  app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);   // Code executed inside Node event loop //
-  res.send('Hi there!');
-});
-
-app.listen(3000);
